2760cc15a5eb1b71a46b3178e8daeb5ff6aba637
Matt Primpas <matthewprimpas@gmail.com>
2023-01-02 20:07:52 -0500

    implement morgan and create stylesheet for views

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const morgan = require('morgan');
 
 // express app
 const app = express();
@@ -7,7 +8,13 @@ const app = express();
 app.set('view engine', 'ejs');
 
 // listen for requests
-app.listen(3000)
+app.listen(3000);
+
+// middleware and static files
+
+app.use(express.static('public'));
+
+app.use(morgan('dev'));
 
 app.get('/', (req, res) => {
     const blogs = [
